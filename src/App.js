6bbf03f1601dc94be260af030e53797a64cb760b
yanxu2021 <yan.xu2021@outlook.com>
2021-07-26 16:45:19 -0700

    Complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["⍰", "⍰", "⍰", "⍰", "⍰", "⍰", "⍰", "⍰", "⍰"],
+      treasureLocation: null
     }
   }
 
+    getTreasure(){
+      let treasure = Math.floor(Math.random() * this.state.board.length)
+      this.setState({treasureLocation:treasure})
+    }
+
+    handleGame = (index) =>{
+      const {board} = this.state
+      if(index === this.state.treasureLocation){
+        board[index] = "💎"
+        this.setState({board:board})
+      }else{
+        board[index] = "🧩"
+        this.setState({board:board})
+      }
+    }
+
   render(){
+    console.log(this.state.treasureLocation)
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div id = "gameboard">
+          {this.state.board.map((value , index) =>{
+          return (
+          <Square 
+          value = {value} 
+          key = {index} 
+          index = {index}
+          handleGame = {this.handleGame} 
+          />
+          )
+        })}
+        </div>
       </>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
 class Square extends Component{
+
+  handleClick = () =>{
+    this.props.handleGame(this.props.index)
+  }
+
   render(){
     return(
       <>
+      <div id = "square" onClick ={this.handleClick}>
+        {this.props.value}
+      </div>
       </>
     )
   }
